0f7f9cb541b2f725ed4ded8e08846d76712d0dbb
Haddad <0418820711@uscis.dhs.gov>
2022-12-30 10:10:33 -0500

    -added various features including looking into fractions. fixed bug where total count was getting the same varialble as count. began to solve search space problems regarding looping through possible variable choices for when sympy doesnt find a solution. fixed bug where total count variables werent contributing to count of unsolved variables. still didn't fix substitution of correct value back into total count I don't think

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ import numpy as np
 
 from itertools import product
 from string import ascii_lowercase
-
-
+from fractions import Fraction
 """
 INPUTS: 
     
@@ -90,7 +89,7 @@ def fillDf(df):
     df=df.loc[df['Subject']!="ELA"]
     missingValues=genMissingValues()
     for value in missingValues:
-        df=df.replace(value, np.NaN)
+        df[['Total Count', 'Count']]=df[['Total Count', 'Count']].replace(value, np.NaN)
     df['Total Count']=df.groupby(['School Name', 'Tested Grade/Subject'])['Total Count'].ffill()
     df['Total Count']=df.groupby(['School Name', 'Tested Grade/Subject'])['Total Count'].bfill()
     for schoolName in df['School Name'].unique():
@@ -106,7 +105,7 @@ def fillDf(df):
                     missingindex=df.loc[(df['School Name']==schoolName) & (df['Metric Value']==j) & (df['Total Count'].isnull())].index[0]
                     df.at[missingindex, 'Total Count']=missing    
             except:
-                print(schoolName)
+                pass
     df['Total Count']=df.groupby(['School Name', 'Tested Grade/Subject'])['Total Count'].ffill()
     df['Total Count']=df.groupby(['School Name', 'Tested Grade/Subject'])['Total Count'].bfill()
     df=df.fillna(-1)
@@ -116,7 +115,7 @@ def fillDf(df):
 
 def replaceWithSymbols(schoolData, mySymbols):    
     schoolData['Count']=schoolData.apply(lambda x: substituteSymbol(x['Count'], x['countWithinSchool'], mySymbols), axis=1)
-    schoolData['Total Count']=schoolData.apply(lambda x: substituteSymbol(x['Total Count'], x['countWithinSchool'], mySymbols), axis=1)
+    schoolData['Total Count']=schoolData.apply(lambda x: substituteSymbol(x['Total Count'], x['totalCountWithinSchool'], mySymbols), axis=1)
     schoolData=schoolData.drop(columns=['countWithinSchool', 'totalCountWithinSchool'])
     return(schoolData)
 
@@ -171,7 +170,7 @@ def proficiencySumByGrade(df, equationDict, number):
 
 def allsClean(df, schoolNumber):
     keywords = [''.join(i) for i in product(ascii_lowercase, repeat = 2)] # you can make more symbols, but this is really what holds up the function and takes tons of time
-    AandB=[i for i in keywords if i[0]=="a" or i[0]=="b"]
+    AandB=[i for i in keywords if i[0]=="a" or i[0]=="b" or i[0]=="c" or i[0]=="d"]
     myString=",".join(AandB)
     schoolString=myString.replace(",",f'{str(schoolNumber)},')+str(schoolNumber)
     mySymbols=symbols(schoolString, integer=True, nonnegative=True)
@@ -262,30 +261,21 @@ def getOrGenData(gen=False):
     levelsAndProf = pd.read_pickle("levelsAndProf.pkl") 
     return(levelsAndProf)
 
-os.chdir(r"C:\Users\aehaddad\Documents")
-
-levelsAndProf=getOrGenData(gen=False)
-testInitialComplete(levelsAndProf)
-brokenSchools=[]
-
-fullListDF=[]
-schoolNumber=0
-
-schoolNames=list(levelsAndProf['School Name'].unique())
-
-for schoolName in schoolNames:  
-    schoolData=levelsAndProf.loc[levelsAndProf['School Name']==schoolName]
-    try:
-        schoolData=allForSchool(schoolData, schoolName, schoolNumber) 
-    except:
-        brokenSchools.append(schoolName)
-    fullListDF.append(schoolData)
-    schoolNumber=schoolNumber+1
-
-fullDF=pd.concat(fullListDF)
-figureOutSums(fullDF)
-#print(fullDF.loc[fullDF['School Name']=="Sela PCS"]['Count'])
-schoolData=fullDF.loc[fullDF['School Name']=="Sela PCS"]
+def iterateThroughSchoolsSolve(levelsAndProf):
+    brokenSchools=[]
+    fullListDF=[]
+    schoolNumber=0
+    schoolNames=list(levelsAndProf['School Name'].unique())
+    for schoolName in schoolNames:  
+        schoolData=levelsAndProf.loc[levelsAndProf['School Name']==schoolName]
+        try:
+            schoolData=allForSchool(schoolData, schoolName, schoolNumber) 
+        except:
+            brokenSchools.append(schoolName)
+        fullListDF.append(schoolData)
+        schoolNumber=schoolNumber+1
+    fullDF=pd.concat(fullListDF)
+    return(fullDF, brokenSchools)
 
 def genMetricsBySchool(levelsAndProf):
     metricsBySchool=[]
@@ -297,28 +287,19 @@ def genMetricsBySchool(levelsAndProf):
     metricDF=pd.DataFrame(metricsBySchool, columns=['School', 'Line Count', 'Missing Count'])
     return(metricDF)
 
-initialCount=genMetricsBySchool(levelsAndProf)
-print(initialCount['Missing Count'].sum())
-finalCount=genMetricsBySchool(fullDF)
-print(finalCount['Missing Count'].sum())
-
-missingCounts=[]
-for schoolName in fullDF['School Name'].unique():
+def determineNumberMissingSymbols(fullDF):
+    missingCounts=[]
+    for schoolName in fullDF['School Name'].unique():
     # this does not work FYI but also we don't really need it
-    #schoolName="Sela PCS"
-    sample=fullDF.loc[fullDF['School Name']==schoolName]
-    symbols=set([i for i in sample['Count'].values if type(i)==sympy.core.symbol.Symbol])
-    missingCounts.append([schoolName, len(symbols)])
+        sample=fullDF.loc[fullDF['School Name']==schoolName]
+        symbols=set([i for i in sample['Count'].values if type(i)==sympy.core.symbol.Symbol] + [i for i in sample['Total Count'].values if type(i)==sympy.core.symbol.Symbol])
+        missingCounts.append([schoolName, len(symbols)])
+    dfmissing=pd.DataFrame(missingCounts)
+    dfmissing.columns=['School Name', 'Count of Missing Symbols']
+    return(dfmissing)
 
-dfmissing=pd.DataFrame(missingCounts)
-dfmissing.columns=['School Name', 'Count of Missing Symbols']
 
-def solveIfNMissing(fullDF, schoolName):
-    possibleValues=[]
-    sample=fullDF.loc[fullDF['School Name']==schoolName]
-    unsolvedValues=[i for i in sample['Count'].values if type(i)!=int]
-    myVars=list(set([i for i in unsolvedValues if type(i)==sympy.core.symbol.Symbol]))
-    allCombos=[i for i in product(range(0,20), repeat = len(myVars))]
+def testCombos(possibleValues, allCombos, unsolvedValues, myVars):
     for aSet in allCombos:
         values=[]
         dictionaryOfValues={}
@@ -330,20 +311,55 @@ def solveIfNMissing(fullDF, schoolName):
         if all(x >= 0 for x in values):
             possibleValues.append(aSet)
     return(myVars, possibleValues) 
+    
+    
+def solveIfNMissing(fullDF, schoolName):
+    print(schoolName)
+    possibleValues=[]
+    sample=fullDF.loc[fullDF['School Name']==schoolName]
+    unsolvedValues=list(set([i for i in sample['Count'].values if type(i)!=int] + [i for i in sample['Total Count'].values if type(i)!=int] ))
+    myVars=list(set([i for i in unsolvedValues if type(i)==sympy.core.symbol.Symbol]))
+    
+    # we need to scope this
+    """
+    there are a couple potential ways of doing this
+    one would be to do so based on the property that the area of possible values is continuous
+    so if there's a 0 that works, and then 1 doesn't, then 2 won't
+    
+    another is to do so based on the values of the non-missing values -- if the total is 20, and we already have a 9 and an 8, then
+    each remaining value has got to be under 4.
+    
+    I think we can do the budget version of #1, where we start with 0-5, and if either no values are found or if one value is a 5 ,we keep going
+    """
+    keepGoing=1
+    for number in [i * 5 for i in range(1, 5)]:
+        if keepGoing==1:
+            allCombos=[i for i in product(range(0,number), repeat = len(myVars))]
+            myVars, possibleValues=testCombos(possibleValues, allCombos, unsolvedValues, myVars)
+            if len([i for i in possibleValues if number in i])<1 and len(possibleValues)>0:
+                keepGoing=0
+    if len(possibleValues)<1:
+        print(f"hmm {schoolName}")
+    return(myVars, possibleValues) 
 
 def getSolvesOnes(fullDF, dfmissing):
-    selectedSchools=dfmissing.loc[dfmissing['Count of Missing Symbols']<3]['School Name']
+    selectedSchools=dfmissing.loc[(dfmissing['Count of Missing Symbols']<7) & (dfmissing['Count of Missing Symbols']>0)]['School Name']
     # this takes awhile to run
     dictOfReplacements={}
+    listOfPossible=[]
     for schoolName in selectedSchools:
         myVars, possibleValues=solveIfNMissing(fullDF, schoolName)
-        if len(possibleValues)==1:
-            replacements=possibleValues[0]
-            for item in range(0, len(myVars)):
-                dictOfReplacements[myVars[item]]=replacements[item]       
+        schoolDF=pd.DataFrame(possibleValues, columns=myVars)
+        listOfPossible.append(schoolDF)
+    allPossible=pd.concat(listOfPossible)
+    uniques=allPossible.nunique()
+    justOneOption=uniques.loc[uniques==1]
+    dfValues=allPossible[justOneOption.index]
+    for col in dfValues.columns:
+        value=[i for i in dfValues[col].values if str(i)!="nan"][0]
+        dictOfReplacements[col]=value
     return(dictOfReplacements)
 
-dictOfReplacements=getSolvesOnes(fullDF, dfmissing)
 
 def applydict(nonintvalue, dictOfReplacements):
     if type(nonintvalue)!=int:
@@ -355,23 +371,203 @@ def applydict(nonintvalue, dictOfReplacements):
     else:
         return(nonintvalue)
     
+os.chdir(r"C:\Users\aehaddad\Documents")
+levelsAndProf=getOrGenData(gen=False)
+testInitialComplete(levelsAndProf)
+initialCount=genMetricsBySchool(levelsAndProf)
+fullDF, brokenSchools=iterateThroughSchoolsSolve(levelsAndProf)
 figureOutSums(fullDF)
+
+print(initialCount['Missing Count'].sum())
+finalCount=genMetricsBySchool(fullDF)
+print(finalCount['Missing Count'].sum())
+
+
+dfmissing=determineNumberMissingSymbols(fullDF)
+
+dictOfReplacements=getSolvesOnes(fullDF, dfmissing)
+
 
 # of new ones we filled in 
 
 fullDF['Test Count']=fullDF.apply(lambda x: applydict(x['Count'], dictOfReplacements), axis=1)     
 byGrade=fullDF.loc[(fullDF['file']=="All") & (fullDF['Tested Grade/Subject']!="All")]
 print(sum([i for i in list(byGrade['Test Count']) if type(i)==int])-sum([i for i in list(byGrade['Count']) if type(i)==int]))
+fullDF['Count']=fullDF['Test Count']
+
+# can we get anything from percentages?
+
+dfmissing=determineNumberMissingSymbols(fullDF)
 
 
+# do we have count/percent pairs where percent is non-missing (not -1) but where there is a non-int value for the Count
 
-# so we are getting slghtly more
+# non-missing percents
+nonMissingPercent=fullDF.loc[fullDF['Percent']!=-1]
 
-# it's working
+nonMissingPercentMissingValue=determineNumberMissingSymbols(nonMissingPercent) # not accurate!!!
+
+valuesToLookAt=[i for i in nonMissingPercent['Count'].values if type(i)!=int]
+rowswithpercent=nonMissingPercent.loc[nonMissingPercent['Count'].isin(valuesToLookAt)]
+# this unfortunately has some <5%s 
+rowswithpercent=rowswithpercent.loc[~rowswithpercent['Percent'].str.contains("<")]
+schoolExample=rowswithpercent.iloc[0]['School Name']
+
+subsetDF=fullDF.loc[fullDF['School Name']==schoolExample]
+
+def solveFractionWithDenominator(count, percent, total_count):
+    # this takes fractions and looks at the total count
+    # this is only going to work if there isn't a weird rounding thing happening
+    # we could make it more flexible if we needed to
+    if type(count)!=int and  percent!=-1:
+        try:
+            target=float(percent)/100
+            denominator=total_count
+            lowest_possible=Fraction(int(round(target*denominator)), denominator)
+            if lowest_possible.denominator==denominator:
+                return( lowest_possible.numerator)
+            elif denominator % lowest_possible.denominator==0:
+                return(denominator/lowest_possible.denominator * lowest_possible.numerator)   
+            else: 
+                print("something weird happened with fraction rounding!")
+                return(-1)
+        except:
+            return(-1)
+    else:
+        return(-1)
+    
+def solveFractionWithDenominatorGetVar(count, percent, total_count):
+    # this is only going to work if the resulting equation has only one variable
+    # this takes fractions and looks at the total count
+    # this is only going to work if there isn't a weird rounding thing happening
+    # we could make it more flexible if we needed to
+    if type(count)!=int and  percent!=-1:
+        try:
+            target=float(percent)/100
+            denominator=total_count
+            lowest_possible=Fraction(int(round(target*denominator)), denominator)
+            #print([count, percent, total_count])
+            if lowest_possible.denominator==denominator:
+                newCount=( lowest_possible.numerator)
+            elif denominator % lowest_possible.denominator==0:
+                newCount=(denominator/lowest_possible.denominator * lowest_possible.numerator)  
+            else: 
+                print("something weird happened with fraction rounding!")
+            #print(newCount)
+            equation=count-newCount
+            #print(equation)
+            #print()
+            return(equation)
+        except:
+            pass
+        
+           
+    
+fullDF['solved_percentage']=fullDF.apply(lambda x: solveFractionWithDenominator(x['Count'], x['Percent'], x['Total Count']), axis=1)
+fullDF.loc[fullDF['solved_percentage']!=-1]
+equations=fullDF.apply(lambda x: solveFractionWithDenominatorGetVar(x['Count'], x['Percent'], x['Total Count']), axis=1)
+actualEquations=[i for i in equations if i]
+
+# this only works because of a quirk in how the library processes symbols, 
+# where there will never be a value of x- 4 without another row having a value of x
+mySymbols=set([i for i in fullDF['Count'].values if type(i)==sympy.core.symbol.Symbol])
+
+solutions = solve(actualEquations, mySymbols, dict=True)
+# we're getting something weird and broken with actualEquations[2] math az179 - 7.0
+if len(solutions)>0:
+    print("number of people we found via fraction trick")
+    fullDF['Test Test Count']=fullDF.apply(lambda x: applydict(x['Count'], solutions[0]), axis=1)     
+    byGrade=fullDF.loc[(fullDF['file']=="All") & (fullDF['Tested Grade/Subject']!="All")]
+    print(sum([i for i in list(byGrade['Test Test Count']) if type(i)==int])-sum([i for i in    list(byGrade['Count']) if type(i)==int]))
+    fullDF['Count']=fullDF['Test Test Count'] 
+else:
+    print("no fraction trick solutions")
+
+
+# so we can see the answer to this is 3/21 -- we have a total count! 
+
+fullDF.loc[fullDF['Percent']=="<=10%"][['Count','Total Count']]
+
+# what we learn from this is that <=10% sometimes includes <5%
+# this still looks plausibly a thing
+
+fullDF.loc[fullDF['Percent'].str.contains("<5")][['Count', 'Total Count', 'Percent']]
+listOfNonIntValues=[i for i in fullDF['Count'].values if type(i)!=int]
+fullDF.loc[(fullDF['Percent'].str.contains("%")) & (fullDF['Count'].isin(listOfNonIntValues))]['School Name'].unique()
+# there's really not much here with the <percentages
+# in part because we're only seeing them in schools that are super missing
+# for comparing with state totals
+
+solvedByGrade=fullDF.loc[(fullDF['Metric Value']=="4 and 5") &  (~fullDF['Count'].isin(listOfNonIntValues))]
+solvedByGrade['Count']=solvedByGrade['Count'].astype(float)
+totalsProf=solvedByGrade.groupby('Tested Grade/Subject').sum()['Count']
+
+# let's look at what's going on at schools with Algebra
+"""
+algebraSchools=fullDF.loc[fullDF['Tested Grade/Subject']=="Algebra I"]['School Name'].unique()
+
+# Browne Education Campus - should have figured out that everything that's not solved for math for count for proficiency is 0
+# Eliot-Hine Middle School is an example of having the overall proficiency count but not algebra
+# there are other schools like Browne -- see Johnson Middle School, Kramer Middle School
+# maybe we didn't try because they have too many variables
+# also we're understating the effect of this process because it's solving almost more variables than people
+# yes! 5-6, 9
+# this is working now but it takes a long time to run because we're not very efficienctly searching the sample space
+# I think to make expanding this possible we need to scope this based on the possible size based on the school
 
 
 
 """
+
+# do we have any solutions where we solved one variable but not another?
+# we're interested in whether the original 'values' say anything about the actual value
+
+# we're going to get this by reading in the original and merging it
+"""
+schoolFile='[5] 2021-22 School Level PARCC and MSAA Data edited.xlsx'
+schoolProficientTab="Proficiency"
+schoolProficientData=genCleanSchool(schoolFile, schoolProficientTab)
+schoolProficientData['file']="Proficiency"
+joinCols=['Tested Grade/Subject',
+ 'file',
+ 'School Name']
+schoolProficientData=schoolProficientData.rename(columns={"Count": "Count missing", "Total Count": "Total count missing"}).drop(columns=["Percent"])
+schoolProficientData=schoolProficientData.loc[schoolProficientData['Subject']!="ELA"]
+
+tellWhatThings=schoolProficientData.merge(fullDF, left_on=joinCols, right_on=joinCols, how="inner")
+
+for value in ["DS", "n<10", "<=10%"]:
+    subset=tellWhatThings.loc[tellWhatThings['Count missing']==value]
+    print(value)
+    items=subset['Count'].unique()
+    print([i for i in items if type(i)==int])
+    # n<10 does actually seem to be n<10
+    
+schoolLevelTab="Performance Level"
+subsetAll=cleanSchoolLevels(schoolFile, schoolLevelTab)
+
+subsetAll['file']="All"
+joinCols=['Tested Grade/Subject',
+ 'file',
+ 'School Name']
+subsetAll=subsetAll.rename(columns={"Count": "Count missing", "Total Count": "Total count missing"}).drop(columns=["Percent"])
+subsetAll=subsetAll.loc[subsetAll['Subject']!="ELA"]
+
+tellWhatThingsTwo=subsetAll.merge(fullDF, left_on=joinCols, right_on=joinCols, how="inner")
+
+for value in ["DS", "n<10", "<=10%"]:
+    subset=tellWhatThingsTwo.loc[tellWhatThingsTwo['Count missing']==value]
+    print(value)
+    items=subset['Count'].unique()
+    #print([i for i in items if type(i)==int])
+    print(len([i for i in items if type(i)!=int]))
+    # n<10 does actually seem to be n<10
+    
+    
+
+fullDF
+
+
         #totalVars=len([i for i in list(solved.values())])
         #unsolvedVars=len([i for i in list(solved.values()) if type(i)==sympy.core.add.Add])
         #toAppend=[schoolName, totalVars, unsolvedVars]
